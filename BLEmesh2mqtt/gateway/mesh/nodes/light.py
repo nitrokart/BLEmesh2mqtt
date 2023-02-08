cda3f4bcc04345ce8bd4831bfee28fadf2ba1ff6
Andre Mariano <nitrokart@gmail.com>
2023-02-08 02:29:50 +0000

    0

---
+++ BLEmesh2mqtt/gateway/mesh/nodes/light.py
@@ -0,0 +1,126 @@
+import asyncio
+import logging
+
+from .generic import Generic
+
+from bluetooth_mesh import models
+
+
+class Light(Generic):
+    """
+    Generic interface for light nodes
+
+    Tracks the available feature of the light. Currently supports
+        - GenericOnOffServer
+            - turn on and off
+        - LightLightnessServer
+            - set brightness
+        - LightCTLServer
+            - set color temperature
+
+    For now only a single element is supported.
+    """
+
+    OnOffProperty = "onoff"
+    BrightnessProperty = "brightness"
+    TemperatureProperty = "temperature"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._features = set()
+
+    def supports(self, property):
+        return property in self._features
+
+    async def turn_on(self):
+        await self.set_onoff_unack(True, transition_time=0.5)
+
+    async def turn_off(self):
+        await self.set_onoff_unack(False, transition_time=0.5)
+
+    async def set_brightness(self, brightness):
+        if self._is_model_bound(models.LightLightnessServer):
+            await self.set_lightness_unack(brightness, transition_time=0.5)
+        elif self._is_model_bound(models.LightCTLServer):
+            await self.set_ctl_unack(brightness=brightness)
+
+    async def set_kelvin(self, temperature):
+        if self._is_model_bound(models.LightCTLServer):
+            await self.set_ctl_unack(temperature)
+
+    async def set_mireds(self, temperature):
+        if self._is_model_bound(models.LightCTLServer):
+            await self.set_ctl_unack(1000000 // temperature)
+
+    async def bind(self, app):
+        await super().bind(app)
+
+        if await self.bind_model(models.GenericOnOffServer):
+            self._features.add(Light.OnOffProperty)
+            await self.get_onoff()
+
+        if await self.bind_model(models.LightLightnessServer):
+            self._features.add(Light.OnOffProperty)
+            self._features.add(Light.BrightnessProperty)
+            await self.get_lightness()
+
+        if await self.bind_model(models.LightCTLServer):
+            self._features.add(Light.TemperatureProperty)
+            self._features.add(Light.BrightnessProperty)
+            await self.get_ctl()
+
+    async def set_onoff_unack(self, onoff, **kwargs):
+        self.notify(Light.OnOffProperty, onoff)
+
+        client = self._app.elements[0][models.GenericOnOffClient]
+        await client.set_onoff_unack(self.unicast, self._app.app_keys[0][0], onoff, **kwargs)
+
+    async def get_onoff(self):
+        client = self._app.elements[0][models.GenericOnOffClient]
+        state = await client.get_light_status([self.unicast], self._app.app_keys[0][0])
+
+        result = state[self.unicast]
+        if result is None:
+            logging.warn(f"Received invalid result {state}")
+        elif not isinstance(result, BaseException):
+            self.notify(Light.OnOffProperty, result["present_onoff"])
+
+    async def set_lightness_unack(self, lightness, **kwargs):
+        self.notify(Light.BrightnessProperty, lightness)
+
+        client = self._app.elements[0][models.LightLightnessClient]
+        await client.set_lightness_unack(self.unicast, self._app.app_keys[0][0], lightness, **kwargs)
+
+    async def get_lightness(self):
+        client = self._app.elements[0][models.LightLightnessClient]
+        state = await client.get_lightness([self.unicast], self._app.app_keys[0][0])
+
+        result = state[self.unicast]
+        if result is None:
+            logging.warn(f"Received invalid result {state}")
+        elif not isinstance(result, BaseException):
+            self.notify(Light.BrightnessProperty, result["present_lightness"])
+
+    async def set_ctl_unack(self, temperature=None, brightness=None, **kwargs):
+        if temperature:
+            self.notify(Light.TemperatureProperty, temperature)
+        else:
+            temperature = self.retained(Light.TemperatureProperty, 255)
+        if brightness:
+            self.notify(Light.BrightnessProperty, temperature)
+        else:
+            brightness = self.retained(Light.BrightnessProperty, 100)
+
+        client = self._app.elements[0][models.LightCTLClient]
+        await client.set_ctl_unack(self.unicast, self._app.app_keys[0][0], temperature, brightness, **kwargs)
+
+    async def get_ctl(self):
+        client = self._app.elements[0][models.LightCTLClient]
+        state = await client.get_ctl([self.unicast], self._app.app_keys[0][0])
+
+        result = state[self.unicast]
+        if result is None:
+            logging.warn(f"Received invalid result {state}")
+        elif not isinstance(result, BaseException):
+            print(result)
---
+++ BLEmesh2mqtt/gateway/mqtt/bridge.py
@@ -0,0 +1,69 @@
+import json
+import logging
+import asyncio
+
+
+class HassMqttBridge:
+    """
+    Base class for all MQTT messenger bridges
+
+    Instances of this class are responsible for bridging between a Home Assistant
+    device type and a specific type of Bluetooth Mesh node.
+
+    This class provides default implementations, that should work for most common nodes.
+    They can however be overriden, if more sophisticated behaviour is required.
+    """
+
+    def __init__(self, messenger):
+        self._messenger = messenger
+
+    @property
+    def component(self):
+        return None
+
+    def _property_change(self, node, property, value):
+        try:
+            # get handler from property name
+            handler = getattr(self, f"_notify_{property}")
+        except:
+            logging.warning(f"Missing handler for property {property}")
+            return
+
+        # TODO: track task
+        asyncio.create_task(handler(node, value))
+
+    async def listen(self, node):
+        """
+        Listen for incoming messages and node changes
+        """
+
+        # send node configuration for MQTT discovery
+        await node.ready.wait()
+        await self.config(node)
+
+        # listen for node changes (this will also push the initial state)
+        node.subscribe(self._property_change, resend=True)
+
+        # listen for incoming MQTT messages
+        async with self._messenger.filtered_messages(self.component, node) as messages:
+            async for message in messages:
+                logging.info(f"Received message on {message.topic}:\n{message.payload}")
+
+                # get command from topic and load message
+                command = message.topic.split("/")[-1]
+                payload = json.loads(message.payload.decode())
+
+                try:
+                    # get handler from command name
+                    handler = getattr(self, f"_mqtt_{command}")
+                except:
+                    logging.warning(f"Missing handler for command {command}")
+                    continue
+
+                await handler(node, payload)
+
+    async def config(self, node):
+        """
+        Send discovery message
+        """
+        pass
