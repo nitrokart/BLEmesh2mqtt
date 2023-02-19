12e547830d3095a828af834ef3972ebb01b27149
Andre Mariano <nitrokart@gmail.com>
2023-02-19 03:23:37 +0000

    alpha1

--- BLEmesh2mqtt/ble/scripts/entrypoint.sh
+++ BLEmesh2mqtt/ble/scripts/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+#Starting BLEmesh2mqtt
+
+who
+
 service dbus start
+
+/usr/libexec/bluetooth/bluetooth-meshd
+
+python3 gateway.py --basedir /config --reload
+
 /usr/libexec/bluetooth/bluetooth-meshd &
 
 python3 gateway.py --basedir /config --reload &
